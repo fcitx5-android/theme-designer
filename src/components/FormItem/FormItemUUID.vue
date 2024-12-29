fa23e2d3938c0695d26d0992352aed0e6b27ba7f
Rocka <i@rocka.me>
2024-12-29 14:48:57 +0800

    fixup

--- src/components/FormItem/FormItemUUID.vue
+++ src/components/FormItem/FormItemUUID.vue
@@ -10,21 +10,21 @@ const emit = defineEmits<{
     update: [value: string]
 }>();
 
-const newUUID = (e: MouseEvent) => {
-    const uuid = uuidv4();
-    emit('update', uuid);
+const newUUID = () => emit('update', uuidv4());
+
+const onClick = (e: MouseEvent) => {
     if ((e as PointerEvent).pointerType === '') { // clicked on the <label>
-        emit('update', uuidv4());
+        newUUID();
     }
 };
 
-const updateOnInput = (e: Event) => {
+const onInput = (e: Event) => {
     emit('update', (e.target as HTMLInputElement).value);
 };
 </script>
 
 <template>
-    <input :id="name" type="text" :value="value" @input="updateOnInput">
+    <input :id="name" type="text" :value="value" @click="onClick" @input="onInput">
     <button @click="newUUID">New UUID</button>
 </template>
 
